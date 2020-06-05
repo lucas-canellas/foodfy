6aeeb174095e812554bca322a11ff96060f3eaba
lucas-canellas <lucascanellas91@gmail.com>
2020-06-04 21:04:44 -0300

    inicio views e rotas admin

--- server.js
+++ server.js
@@ -24,16 +24,49 @@ server.get('/recipes', function (req, res) {
   return res.render("recipes", { items: recipes })
 })
 
-server.get("/recipes/:index", function (req, res) {
-  const index = req.params.index
-  const recipe = recipes[index]
+server.get("/recipes/:id", function (req, res) {
+  
+  const id = req.params.id
+  const recipe = recipes[id]
 
   return res.render("detailrecipe", { items: recipe })
 
 })
 
 
+// ADMIN
+
+
+server.get("/admin/recipes", function(req, res) {
+  return res.render("admin/recipes/index", { items: recipes })
+});
+
+server.get("/admin/recipes/create", function(req, res) {
+  return res.render("admin/recipes/create", { items: recipes })
+});
+
+server.get("/admin/recipes/:id", function (req, res) {
+  
+  const id = req.params.id
+  const recipe = recipes[id]
+
+  return res.render("admin/recipes/show", { items: recipe })
+
+})
+
+server.get("/admin/recipes/:id/edit", function(req, res) {
+
+  const id = req.params.id
+  const recipe = recipes[id]
+
+  return res.render('admin/recipes/edit', {items: recipe})
+})
+
 server.listen(5000, function () {
   console.log("running server...")
 
 })
+
+
+
+
